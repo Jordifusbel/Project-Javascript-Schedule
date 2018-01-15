6cde08f9923f1fbf71385c2385005a0cc791f9d0
Jordi <32875964+Jordifusbel@users.noreply.github.com>
2018-01-15 20:28:40 +0100

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,7 +1,7 @@
 function whiteStyle(x) {
  	var element = document.getElementsByClassName(x);
 	for (var i = 0; i < element.length; i++){
-	element[i].style.color = "#ffffff";
+	element[i].style.color = "lightgreen";
 	/* element[i].style.background-color = "black"; */
 		}
 	}
