052e9ff925840bbfef0b6ba5753179e0a60a037c
Jordi <32875964+Jordifusbel@users.noreply.github.com>
2018-01-17 19:28:57 +0100

    Update JS.js

--- JS.js
+++ JS.js
@@ -12,5 +12,9 @@ x.style.backgroundColor= "tomato"
     
 	
 function showInfo(x) {
+/*var subjList = ["BBDD", "SI", "Prog", "ED", "FOL", "LLMM", "LLMMEx", "ProgEx"]
+for elements in subjList{
+document.getElementById.style.display = "none";	
+}*/
 document.getElementById(x.attributes["class"].value).style.display = "block";
 }
